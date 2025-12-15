055587ed428abdff0862a03568912c27bc525431
Ayodeji Timilehin <TimilehinAyodeji99@gmail.com>
2025-12-15 10:44:38 +0100

    Update covidquery (2).sql

--- covidquery (2).sql
+++ covidquery (2).sql
@@ -1,10 +1,10 @@
--- Select the part of the data that we are going to be working with 
+-- -- Select the part of the data that we are going to be working with 
 
 SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM covid.deaths 
 ORDER BY location, date 
 
--- % of cases that lead to deaths 
+-- -- % of cases that lead to deaths 
 SELECT location, date, total_cases, total_deaths, ROUND((total_deaths/total_cases)*100, 2) as mortality_rate
 FROM covid.deaths 
 WHERE location = 'Nigeria'
@@ -12,7 +12,7 @@ ORDER BY 1,2
 
 
 
--- What percentage of population had covid (Nigeria)
+-- -- What percentage of population had covid (Nigeria)
 SELECT location, population, date, total_cases, ROUND((total_cases/population)*100, 2) AS percentage_cases
 FROM covid.deaths
 WHERE location = 'Nigeria'
@@ -21,7 +21,7 @@ LIMIT 1
 
 
 
--- Countries with highest infetcion rate
+-- -- Countries with highest infetcion rate
 SELECT location, population, MAX(total_cases) AS infection_count, ROUND(MAX(total_cases/population)*100, 2) AS infection_rate
 FROM covid.deaths
 WHERE continent IS NOT NULL
@@ -29,7 +29,7 @@ GROUP BY 1,2
 ORDER BY infection_rate DESC 
 
 
---Countries with the highest mortality rate 
+-- --Countries with the highest mortality rate 
 SELECT location, population, MAX(total_deaths) AS death_count, ROUND(MAX(total_deaths/population)*100, 2) AS death_rate
 FROM covid.deaths
 WHERE continent IS NOT NULL
@@ -45,7 +45,35 @@ GROUP BY 1, 2
 ORDER BY death_count DESC
 
 
--- Deaths by Continent 
+-- -- Query for total deaths. 
+SELECT 
+    SUM(final_deaths) AS death_count
+FROM (
+    SELECT 
+        location, 
+        MAX(total_deaths) AS final_deaths
+    FROM covid.deaths
+    WHERE continent IS NOT NULL
+    GROUP BY location
+) AS t;
+
+
+
+-- -- Query for total cases. 
+SELECT 
+    SUM(worldwide_cases) AS world_cases
+FROM (
+    SELECT 
+        location, 
+        MAX(total_cases) AS worldwide_cases
+    FROM covid.deaths
+    WHERE continent IS NOT NULL
+    GROUP BY location
+) AS ta;
+
+
+
+-- -- Deaths by Continent 
 SELECT location, population, MAX(total_deaths) AS death_count, (Max(total_deaths)/population)*100 AS mortality_rate
 FROM covid.deaths
 WHERE continent IS NULL
@@ -53,7 +81,7 @@ GROUP BY 1, 2
 ORDER BY mortality_rate DESC
 
 
- -- Global Numbers 
+--  -- Global Numbers 
 SELECT date,population, (SUM(new_cases)) AS worldwide_cases, SUM(new_deaths) AS worldwide_deaths
 FROM covid.deaths
 WHERE continent IS NOT NULL
@@ -61,11 +89,36 @@ GROUP BY date, population
 ORDER BY date
 
 
-Combine deaths table with vaccination table
+ -- Query for total tests carried out. 
+SELECT 
+    SUM(final_tests) AS test_count
+FROM (
+    SELECT 
+        location, 
+        MAX(total_tests) AS final_tests
+    FROM covid.vaccinations
+    WHERE continent IS NOT NULL
+    GROUP BY location
+) AS s;
+
+
+-- Query for Total People Vaccinated 
+SELECT SUM(vaccinated_people) AS vacc_people
+FROM(
+  SELECT location, MAX(people_vaccinated) AS vaccinated_people
+  FROM covid.vaccinations
+  WHERE continent IS NOT NULL
+  GROUP BY location)
+AS v
+
+
+-- Combine deaths table with vaccination table
 SELECT *
 FROM covid.deaths as deaths
   Join covid.vaccinations as vacc
   ON deaths.date = vacc.date
+
+
 
 -- Total Population vs Vaccinations: This query gives a result indicating the number of people who got vaccinated on a daily basis (date), per location (country) 
 
@@ -89,6 +142,7 @@ WHERE deaths.continent IS NOT NULL AND deaths.location = "Albania"
 ORDER BY deaths.location, deaths.date
 )
 
--- SELECT *
+SELECT *
+FROM vacc_roll_count
 
--- FROM vacc_roll_count
+
